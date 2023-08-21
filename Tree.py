2ba987ef6fc33d1ee714e03fae36b26312d23401
William Wu <57069501+Williamwu277@users.noreply.github.com>
2023-08-21 17:50:21 -0400

    Update Tree.py
    Added comments

--- Tree.py
+++ Tree.py
@@ -1,10 +1,11 @@
 import random
 from PIL import Image, ImageDraw
 
-
+# tree class
 class Tree:
 
     def __init__(self, params):
+        # parameters
         self.nodes = params[0]
         self.weight = params[1]
         self.key = params[2]
@@ -13,12 +14,15 @@ class Tree:
         if self.nodes > 200:
             raise TimeoutError
 
+        # edge graph, adjacency list, distances list, image
         self.edge_graph = []
         self.adj = [[] for _ in range(self.nodes+1)]
         self.distances = [0 for _ in range(self.nodes+1)]
         self.image = None
 
+        # generate tree based on method
         if self.key == "RANDOM":
+            # random tree generation, average depth of log(N)
             for vert in range(2, self.nodes+1):
                 start = random.randint(1, vert-1)
                 length = random.randint(1, self.weight)
@@ -28,7 +32,9 @@ class Tree:
                     self.edge_graph.append((start, vert, length))
                 else:
                     self.edge_graph.append((start, vert))
+                    
         elif self.key == "LINE":
+            # generate a line graph
             for vert in range(2, self.nodes+1):
                 if random.randint(1, max(self.alt, 1)) == 1:
                     start = random.randint(1, vert-1)
@@ -41,7 +47,9 @@ class Tree:
                     self.edge_graph.append((start, vert, length))
                 else:
                     self.edge_graph.append((start, vert))
+                    
         elif self.key == "STAR":
+            # generate a star graph
             for vert in range(2, self.nodes+1):
                 if random.randint(1, max(self.alt, 1)) == 1:
                     start = random.randint(1, vert-1)
@@ -55,6 +63,7 @@ class Tree:
                 else:
                     self.edge_graph.append((start, vert))
 
+    # output the tree in text
     def __str__(self):
         tree = str(self.nodes)+"\n"
         for nxt in range(self.nodes-1):
@@ -65,25 +74,29 @@ class Tree:
             tree += line+"\n"
         return tree.rstrip()
 
+    # run depth first search to find distances
     def DFS(self, cur, parent, dist):
         self.distances[cur] = dist
         for nxt in self.adj[cur]:
             if nxt[0] != parent:
                 self.DFS(nxt[0], cur, dist + nxt[1])
-    
+
+    # draw the tree out
     def DRAW(self):
         self.image = Image.new("RGB", (400, 400))
 
+        # find the depths of each node
         depth = [0 for _ in range(self.nodes+1)]
         def dfs(cur, par):
             for nxt in self.adj[cur]:
                 if nxt[0] != par:
                     depth[nxt[0]] = depth[cur] + 1
                     dfs(nxt[0], cur)
-
+        
         dfs(1, -1)
         mp = dict()
         other = 0
+        # count number of nodes per depth
         for i in range(1, self.nodes+1):
             if depth[i] in mp.keys():
                 mp[depth[i]] += 1
@@ -93,12 +106,14 @@ class Tree:
         for key in mp.keys():
             cnt[key] = mp[key]
 
+        # calculate scale
         high = max(depth)+1
         other = max(mp.values())
         size = 400 // max(high*2+1, other*2+1, 1)
         other = 400 // (other*2+1)
         high = 400 // (high*2+1)
 
+        # draw the tree
         def dfs_draw(cur, par, linexy, wei):
 
             val = 400 // (cnt[depth[cur]]+1)
@@ -126,4 +141,3 @@ class Tree:
             mp[depth[cur]] -= 1
 
         dfs_draw(1, -1, None, -1)
-        #image.save("graph.png")
